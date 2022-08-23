b28e38b41c67a6796009d6fd78852b50ea2fdc43
meng <monkeystylle@gmail.com>
2022-08-24 00:36:16 +0800

    added Navbar, Spacer, Layout Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <>
-      <Title>My page ATUA</Title>
+      <Title>This is a Sample Page</Title>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
         neque, ab eligendi natus nostrum optio.
