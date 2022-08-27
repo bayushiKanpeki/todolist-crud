bfcbc5f0463bb1ce3b09b4935639063abfa26363
meng <monkeystylle@gmail.com>
2022-08-28 05:31:16 +0800

    added Roboto font

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -108,6 +108,8 @@ body {
   
   font-family:   -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  font-family: 'Roboto', sans-serif;
+
 }
 
 
